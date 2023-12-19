50433f61a48469c77e76963f6d2d264097ca85dc
Jaya Krishna <namburu1995@gmail.com>
2023-12-19 16:29:36 +0530

    (fix): Expose individual types for swiper options and events

--- src/swiper.d.ts
+++ src/swiper.d.ts
@@ -1,5 +1,9 @@
 // @ts-ignore
 import Swiper from './types/swiper-class.d.ts';
+// @ts-ignore
+export * from './types/swiper-options.d.ts';
+// @ts-ignore
+export * from './types/swiper-events.d.ts';
 
 export default Swiper;
 export { Swiper };
